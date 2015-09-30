5cf9662286cbe74bf2fd567d4c66a85330b7ddb8
RobIsRight <victor_selvaraj@hotmail.com>
2015-09-30 09:11:17 -0500

    finished project

--- PowerDigitSum/PowerDigitSum.cpp
+++ PowerDigitSum/PowerDigitSum.cpp
@@ -2,31 +2,56 @@
 //
 
 #include "stdafx.h"
-
+#include <conio.h>
+//Power digit sum
+//Problem 16
+//2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+//
+//What is the sum of the digits of the number 2^1000 ?
 
 int main(int argc, _TCHAR* argv[])
 {
-	int NumField[400];
+	
 	int ProductField[400];
-
-	NumField[0] = 2;
-	int pos1  = 0;
-	int pos2 = 0;
-	int carry = 0;
-	int product = 0;
-	int sumOfDigits = 0;
-	for (int i = 1; i <= 1000; i++)
+	for (int counter = 0; counter < 400; counter++)
 	{
-		product = NumField[pos1] * 2;
-		carry = product / 10;
-		ProductField[pos2] = product % 10;
-		ProductField[pos2 + 1] = carry + ProductField[pos2 + 1];
+		
+		ProductField[counter] = 0;
 	}
 
 
+	ProductField[0] = 2;
+	int carry = 0;
+	int product = 0;
+	long long sumOfDigits = 0;
+	int ProductDigitSize = 1;
+	for (int i = 2; i <= 1000; i++)
+	{
+		carry = 0;
+		product = 0; 
+		int j = 0;
+		for ( j = 0; j < ProductDigitSize; j++)
+		{
+			product = (ProductField[j] * 2) + carry;
+			carry =  product / 10;
+			ProductField[j] = product % 10;
+			
+		}
+		if (carry != 0)
+		{
+			ProductField[j] = carry;
+			ProductDigitSize++;
+		}
+		
+	}
 
-	//fprintf(stdout, " The sum of Digits of 2^1000 is %d", sum);
-	
+	for (int i = 0; i < ProductDigitSize; i++)
+	{
+		sumOfDigits = sumOfDigits + ProductField[i];
+	}
+
+	fprintf(stdout, " The sum of Digits of 2^1000 is %d", sumOfDigits);
+	_getch();
 	return 0;
 }
 
